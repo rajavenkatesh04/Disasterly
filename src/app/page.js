bf68226169934fbd32a4bd3793a2f27ccb15f3e4
rajavenkatesh04 <rg3941@srmist.edu.in>
2025-03-12 00:54:30 +0530

    Add Analytics tracker

--- src/app/page.js
+++ src/app/page.js
@@ -3,6 +3,7 @@ import Navbar from "@/components/ui/Navbar";
 import HomePage from "@/components/HomePage";
 import Footer from "@/components/ui/Footer";
 
+
 export default function Home() {
   return (
       <div>
